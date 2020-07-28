b570d52eb433fc7e9a4b26f2a95e31ed0f5d16ca
strange15 <strange66666666@gmail.com>
2020-07-28 10:45:53 +0800

    try owl-carousel

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="footer">
       資料、圖片、文章來源皆來自網路，僅用來做為學習用途。
-      <swiper class="swiper" :options="swiperOption">
+      <!-- <swiper class="swiper" :options="swiperOption">
         <swiper-slide><img
             src="../assets/images/index/banner1.jpg"
             class="w-100"
@@ -16,44 +16,54 @@
             class="w-100"
           /></swiper-slide>
         <div class="swiper-pagination" slot="pagination"></div>
-        <!-- <div class="swiper-button-prev" slot="button-prev"></div>
-        <div class="swiper-button-next" slot="button-next"></div> -->
-      </swiper>
+      </swiper>-->
+      <div class="owl-carousel owl-theme">
+        <div>Your Content</div>
+        <div>Your Content</div>
+        <div>Your Content</div>
+        <div>Your Content</div>
+        <div>Your Content</div>
+        <div>Your Content</div>
+        <div>Your Content</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { Swiper, SwiperSlide } from "vue-awesome-swiper";
-import 'swiper/dist/css/swiper.css'
+// import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+// import 'swiper/dist/css/swiper.css'
+import $ from "jquery";
 export default {
   data() {
     return {
-      swiperOption: {
-        spaceBetween: 30,
-        centeredSlides: true,
-        effect: 'fade',
-        autoplay: {
-          delay: 5500,
-          disableOnInteraction: false
-        },
-
-        speed: 3000,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev"
-        }
-      }
+      // swiperOption: {
+      //   spaceBetween: 30,
+      //   centeredSlides: true,
+      //   effect: 'fade',
+      //   autoplay: {
+      //     delay: 5500,
+      //     disableOnInteraction: false
+      //   },
+      //   speed: 3000,
+      //   pagination: {
+      //     el: ".swiper-pagination",
+      //     clickable: true
+      //   },
+      //   navigation: {
+      //     nextEl: ".swiper-button-next",
+      //     prevEl: ".swiper-button-prev"
+      //   }
+      // }
     };
   },
   components: {
-    Swiper,
-    SwiperSlide
-  }
+    // Swiper,
+    // SwiperSlide
+  },
+  created() {
+    $(".owl-carousel").owlCarousel();
+  },
 };
 </script>
 <style lang="less" scoped>
