1b71b97a01553482962949b9fe0b9e9e173bccf1
strange15 <strange66666666@gmail.com>
2020-08-05 13:24:21 +0800

    use owl-carousel, but style not work

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,76 +2,22 @@
   <div>
     <div class="footer">
       資料、圖片、文章來源皆來自網路，僅用來做為學習用途。
-      <!-- <swiper class="swiper" :options="swiperOption">
-        <swiper-slide><img
-            src="../assets/images/index/banner1.jpg"
-            class="w-100"
-          /></swiper-slide>
-        <swiper-slide><img
-            src="../assets/images/index/banner2.jpg"
-            class="w-100"
-          /></swiper-slide>
-        <swiper-slide><img
-            src="../assets/images/index/banner3.jpg"
-            class="w-100"
-          /></swiper-slide>
-        <div class="swiper-pagination" slot="pagination"></div>
-      </swiper>-->
-      <div class="owl-carousel owl-theme">
-        <div>Your Content</div>
-        <div>Your Content</div>
-        <div>Your Content</div>
-        <div>Your Content</div>
-        <div>Your Content</div>
-        <div>Your Content</div>
-        <div>Your Content</div>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
-// import { Swiper, SwiperSlide } from "vue-awesome-swiper";
-// import 'swiper/dist/css/swiper.css'
-// import $ from "jquery";
 export default {
   data() {
     return {
-      // swiperOption: {
-      //   spaceBetween: 30,
-      //   centeredSlides: true,
-      //   effect: 'fade',
-      //   autoplay: {
-      //     delay: 5500,
-      //     disableOnInteraction: false
-      //   },
-      //   speed: 3000,
-      //   pagination: {
-      //     el: ".swiper-pagination",
-      //     clickable: true
-      //   },
-      //   navigation: {
-      //     nextEl: ".swiper-button-next",
-      //     prevEl: ".swiper-button-prev"
-      //   }
-      // }
     };
   },
   components: {
-    // Swiper,
-    // SwiperSlide
   },
-  created() {
-    
-  },
+  created() {},
   mounted() {
-    // let owl_carousel = require('owl.carousel');
-    // window.fn = owl_carousel;
-    $(".owl-carousel").owlCarousel();
-  }
+  },
 };
 </script>
 <style lang="less" scoped>
-.footer {
-}
 </style>
--- src/components/pages/Index.vue
+++ src/components/pages/Index.vue
@@ -3,47 +3,30 @@
     <div class="vld-parent">
       <loading :active.sync="isLoading"></loading>
     </div>
-
-    <div
-      id="carouselExampleIndicators"
-      class="carousel slide carousel-fade"
-      data-ride="carousel"
-    >
+    <!-- bootstrap carousel -->
+    <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
       <ol class="carousel-indicators">
-        <li
-          data-target="#carouselExampleIndicators"
-          data-slide-to="0"
-          class="active"
-        ></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
       </ol>
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img
-            src="../../assets/images/index/banner1.jpg"
-            class="d-block w-100"
-          />
+          <img src="../../assets/images/index/banner1.jpg" class="d-block w-100" />
           <div class="carousel-caption d-none d-md-block">
             <h5>First</h5>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
         </div>
         <div class="carousel-item">
-          <img
-            src="../../assets/images/index/banner2.jpg"
-            class="d-block w-100"
-          />
+          <img src="../../assets/images/index/banner2.jpg" class="d-block w-100" />
           <div class="carousel-caption d-none d-md-block">
             <h5>Second</h5>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
         </div>
         <div class="carousel-item">
-          <img
-            src="../../assets/images/index/banner3.jpg"
-            class="d-block w-100"
-          />
+          <img src="../../assets/images/index/banner3.jpg" class="d-block w-100" />
           <div class="carousel-caption d-none d-md-block">
             <h5>Third</h5>
             <p>Lorem ipsum dolor sit amet.</p>
@@ -69,6 +52,32 @@
         <span class="sr-only">Next</span>
       </a>
     </div>
+
+    <!-- owl-carousel  -->
+    <!-- <div class="owl-carousel owl-theme animated">
+      <div>
+        <img src="../../assets/images/index/banner1.jpg" class />
+      </div>
+      <div>
+        <img src="../../assets/images/index/banner2.jpg" class />
+      </div>
+      <div>
+        <img src="../../assets/images/index/banner3.jpg" class />
+      </div>
+      <div class="owl-controls">
+        <div class="owl-dots">
+          <div class="owl-dot active">
+            <span></span>
+          </div>
+          <div class="owl-dot">
+            <span></span>
+          </div>
+          <div class="owl-dot">
+            <span></span>
+          </div>
+        </div>
+      </div>
+    </div> -->
   </div>
 </template>
 
@@ -77,22 +86,34 @@
 export default {
   data() {
     return {
-      isLoading: false
+      isLoading: false,
     };
   },
   components: {},
   methods: {
     initCarousel() {
+      // bootstrap carousel
       $(".carousel").carousel({
-        interval: 5000
+        interval: 5000,
       });
-    }
+
+      // owl-carousel
+      // $(".owl-carousel").owlCarousel({
+      //   loop: true,
+      //   autoWidth: true,
+      //   animateOut: "fadeOut",
+      //   animateIn: "fadeIn",
+      //   autoplay: true,
+      //   items: 1,
+      //   smartSpeed: 450,
+      // });
+    },
   },
   created() {},
   computed: {},
   mounted() {
     this.initCarousel();
-  }
+  },
 };
 </script>
 <style lang="scss" scoped>
@@ -108,4 +129,17 @@ export default {
     }
   }
 }
+
+// .owl-carousel .owl-item img {
+//     height: 100vh;
+//     object-fit: cover;
+
+//     .owl-controls .owl-dots {
+//       position: absolute !important;
+//       bottom: 1rem;
+//       left: 50%;
+//       transform: translateX(-50%);
+
+//     }
+// }
 </style>
