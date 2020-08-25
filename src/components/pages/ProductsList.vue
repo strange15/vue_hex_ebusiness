ff93524ae34f274ba23faf725f1dd9a42b070888
strange15 <strange66666666@gmail.com>
2020-08-25 17:48:28 +0800

    所有商品頁 新增搜尋功能

--- src/components/pages/ProductsList.vue
+++ src/components/pages/ProductsList.vue
@@ -12,25 +12,41 @@
             <div
               class="list-group-item list-group-item-action"
               :class="{ 'active': active === 'all' }"
-               @click="changeCategory('all')"
+              @click="changeCategory('all')"
             >
               <i></i> 所有商品
             </div>
-            <div class="list-group-item list-group-item-action" @click="changeCategory(category)"
-              v-for="(category, index) in categoryTab" :key="index" 
-              :class="{ 'active': active === category }">
-              <i></i> {{ category }}
+            <div
+              class="list-group-item list-group-item-action"
+              @click="changeCategory(category)"
+              v-for="(category, index) in categoryTab"
+              :key="index"
+              :class="{ 'active': active === category }"
+            >
+              <i></i>
+              {{ category }}
             </div>
           </div>
         </div>
         <div class="col-md-9">
           <div class="d-flex mb-4">
-            <!-- Search bar TODO -->
             <form class="form-inline my-3 my-lg-0">
               <div class="input-group">
-                <input class="form-control" type="text" placeholder="Search" aria-label="Search" />
+                <input
+                  class="form-control"
+                  type="text"
+                  placeholder="搜尋商品名稱"
+                  v-model="searchProd"
+                  aria-label="Search"
+                />
+                <i
+                  v-if="isFilter"
+                  class="far fa-times-circle pointer"
+                  aria-hidden="true"
+                  @click.prevent="clearSearch()"
+                ></i>
                 <div class="input-group-append">
-                  <button class="btn btn-outline-warning" type="submit">
+                  <button class="btn btn-outline-warning" @click.prevent="searchProducts()">
                     <i class="fa fa-search" aria-hidden="true"></i> Search
                   </button>
                 </div>
@@ -38,30 +54,73 @@
             </form>
           </div>
           <div class="tab-content">
-            <div class="">
-              <div class="row">
-                <div class="col-md-4 mb-4" v-for="prod in tmpProducts" :key="prod.id">
-                  <div class="card border-0 box-shadow text-center h-100">
-                    <router-link :to="{ name: 'Product', params: { pid: prod.id }}">
-                      <img
-                        class="card-img-top"
-                        :src="prod.imageUrl"
-                        alt="Card image cap"
-                      />
-                    </router-link>
-                    <div class="card-body">
-                      <h4 class="card-title">{{ prod.title }}</h4>
-                      <p class="card-text bold">
-                        {{ prod.description }}
-                      </p>
-                      <p class="card-text">
-                        {{ prod.content }}
-                      </p>
+            <div class>
+              <div v-if="isFilter">
+                <div class="row">
+                  <div class="col-md-4 mb-4" v-for="prod in filterProducts" :key="prod.id">
+                    <div class="card border-0 box-shadow text-center h-100">
+                      <router-link :to="{ name: 'Product', params: { pid: prod.id }}">
+                        <img class="card-img-top" :src="prod.imageUrl" alt="Card image cap" />
+                      </router-link>
+                      <div class="card-body">
+                        <h4 class="card-title">{{ prod.title }}</h4>
+                        <p class="card-text bold">{{ prod.description }}</p>
+                        <p class="card-text">{{ prod.content }}</p>
+                      </div>
+                      <div class="card-footer border-top-0 bg-white">
+                        <a
+                          class="btn btn-outline-secondary btn-block btn-sm"
+                          @click="addToCart(prod.id)"
+                        >
+                          <i class="fa fa-cart-plus" aria-hidden="true"></i> 加入購物車
+                          <i
+                            class="fas fa-spinner fa-spin"
+                            v-if="status.itemLoading === prod.id"
+                          ></i>
+                        </a>
+                        <!-- TODO 加入我的最愛 -->
+                        <!-- <a
+                            class="btn btn-outline-secondary btn-block btn-sm"
+                            @click="addToFav({control:'add', data:prod})"
+                          >
+                            <i class="fa fa-star" aria-hidden="true"></i> 加入我的最愛
+                        </a>-->
+                      </div>
                     </div>
-                    <div class="card-footer border-top-0 bg-white">
-                      <a class="btn btn-outline-secondary btn-block btn-sm" @click="addToCart(prod.id)">
-                        <i class="fa fa-cart-plus" aria-hidden="true"></i> 加入購物車 <i class="fas fa-spinner fa-spin" v-if="status.itemLoading === prod.id"></i>
-                      </a>
+                  </div>
+                </div>
+              </div>
+              <div v-else>
+                <div class="row">
+                  <div class="col-md-4 mb-4" v-for="prod in tmpProducts" :key="prod.id">
+                    <div class="card border-0 box-shadow text-center h-100">
+                      <router-link :to="{ name: 'Product', params: { pid: prod.id }}">
+                        <img class="card-img-top" :src="prod.imageUrl" alt="Card image cap" />
+                      </router-link>
+                      <div class="card-body">
+                        <h4 class="card-title">{{ prod.title }}</h4>
+                        <p class="card-text bold">{{ prod.description }}</p>
+                        <p class="card-text">{{ prod.content }}</p>
+                      </div>
+                      <div class="card-footer border-top-0 bg-white">
+                        <a
+                          class="btn btn-outline-secondary btn-block btn-sm"
+                          @click="addToCart(prod.id)"
+                        >
+                          <i class="fa fa-cart-plus" aria-hidden="true"></i> 加入購物車
+                          <i
+                            class="fas fa-spinner fa-spin"
+                            v-if="status.itemLoading === prod.id"
+                          ></i>
+                        </a>
+                        <!-- TODO 加入我的最愛 -->
+                        <!-- <a
+                            class="btn btn-outline-secondary btn-block btn-sm"
+                            @click="addToFav({control:'add', data:prod})"
+                          >
+                            <i class="fa fa-star" aria-hidden="true"></i> 加入我的最愛
+                        </a>-->
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -73,8 +132,6 @@
         </div>
       </div>
     </div>
-
-  
   </div>
 </template>
 
@@ -88,11 +145,15 @@ export default {
       products: [],
       categoryTab: [], // 類別
       tmpProducts: [],
+      filterProducts: [], // 搜尋結果
+      searchProd: "", // 搜尋字串
+      isFilter: false,
       isLoading: false,
       status: {
-        itemLoading: ""
+        itemLoading: "",
       },
       pagination: {},
+      favorite: [], // 我的最愛
     };
   },
   // components: { Pagination },
@@ -115,16 +176,16 @@ export default {
     changeCategory(tab) {
       let vm = this;
       vm.active = tab;
-      if( tab === "all" ) {
+      vm.clearSearch();
+      if (tab === "all") {
         vm.tmpProducts = vm.products;
         // return;
-      }else
-        vm.tmpProducts = vm.products.filter(x => x.category === tab);
+      } else vm.tmpProducts = vm.products.filter((x) => x.category === tab);
     },
     /** 整理 categorys */
     filterCategory(categorys) {
       let arr = [];
-      categorys.forEach(function(cate){
+      categorys.forEach(function (cate) {
         arr.push(cate.category);
       });
       this.categoryTab = [...new Set(arr)]; // 過濾重複的值
@@ -132,78 +193,146 @@ export default {
     initTab() {
       this.changeCategory("all");
     },
+    // 搜尋商品
+    searchProducts() {
+      let vm = this;
+      let filterWords = this.searchProd;
+      if (filterWords === "") { vm.isFilter = false; return; }
+      vm.filterProducts = vm.tmpProducts.filter((x) => x.title.indexOf(filterWords) > -1);
+      if (vm.filterProducts.length !== 0) vm.isFilter = true;
+      else{
+        vm.$alert('請嘗試其他關鍵字', '搜尋不到此項產品', {
+          confirmButtonText: '確定',
+        });
+      }
+    },
+    // 清除搜尋欄
+    clearSearch() {
+      this.isFilter = false;
+      this.searchProd = "";
+    },
     // 加入購物車
     addToCart(id, qty = 1) {
       const vm = this;
       vm.status.itemLoading = id;
       const cart = {
         product_id: id,
-        qty
+        qty,
       };
       this.$http
         .post(`${this.API.ADD_TO_CART}`, { data: cart })
-        .then(response => {
+        .then((response) => {
           this.$bus.$emit("shoppingcart:get");
-          // $("#productModal").modal("hide");
           vm.status.itemLoading = "";
         });
+    },
+    // 加入我的最愛
+    addToFav(obj) {
+      /**
+       * obj = {
+       *  control:'remove' or 'add',
+       * data:prod,
+       * }
+       */
+      let vm = this;
+      console.log("addToFav", obj);
+      // 新增我的最愛
+      if (obj.control == "add") {
+        this.favorite.push(obj.data);
+        // this.location = this.location.filter(function (item, index) {
+        //   if (item.SiteId == obj.data.SiteId) {
+        //     return false;
+        //   }
+        //   return true;
+        // });
+        localStorage.setItem("favorite", JSON.stringify(this.favorite));
+        return;
+      }
+
+      // 移除我的最愛
+      // this.favorite = this.favorite.filter(function (item, index) {
+      //   if (item.SiteId == obj.data.SiteId) {
+      //     return false;
+      //   }
+      //   return true;
+      // });
+
+      // 如果移除此最愛後, 我的最愛的陣列是空的, 就直接拿原本的資料來 gen
+      // if (this.favorite.length == 0) {
+      //   localStorage.clear();
+      //   vm.location = vm.data;
+      //   return;
+      // }
+      // 如果我的最愛的陣列內還有值, 再做篩選
+      // vm.favorite.forEach(function (star, i) {
+      //   vm.location = [...vm.data].filter(function (item, index) {
+      //     if (item.SiteId == star.SiteId) return false;
+      //     return true;
+      //   });
+      // });
+      localStorage.setItem("favorite", JSON.stringify(vm.favorite));
     },
   },
   created() {
     this.getProducts();
   },
-  computed: {
-
-  },
+  computed: {},
 };
 </script>
 <style lang="less" scope>
 .container {
-    margin-top: 10rem;
+  margin-top: 10rem;
 
-    .category-group {
-      .list-group-item {
-        border: 0 !important;
-        cursor: pointer;
-        padding-left: 2rem;
+  .category-group {
+    .list-group-item {
+      border: 0 !important;
+      cursor: pointer;
+      padding-left: 2rem;
 
-        &:nth-child(n+2) {
-          margin-top: .8rem;
-        }
-
-        i::before {
-          position: absolute;
-          content: '';
-          left: .5rem;
-          top: 50%;
-          transform: translateY(-44%);
-          width: 8px;
-          height: 15px;
-          background: #202f95;
-        }
+      &:nth-child(n + 2) {
+        margin-top: 0.8rem;
       }
-      .list-group-item.active {
-        background-color: #202f95;
 
-        i::before {
-          position: absolute;
-          left: .5rem;
-          top: 50%;
-          transform: translateY(-44%);
-          width: 8px;
-          height: 15px;
-          background: #fff;
-        }
+      i::before {
+        position: absolute;
+        content: "";
+        left: 0.5rem;
+        top: 50%;
+        transform: translateY(-44%);
+        width: 8px;
+        height: 15px;
+        background: #202f95;
       }
     }
+    .list-group-item.active {
+      background-color: #202f95;
 
-    .btn-outline-secondary:hover {
-      background-color: #202f95 !important;
-      color: #fff;
+      i::before {
+        position: absolute;
+        left: 0.5rem;
+        top: 50%;
+        transform: translateY(-44%);
+        width: 8px;
+        height: 15px;
+        background: #fff;
+      }
     }
+  }
 
-    .bold {
-      font-weight: bold;
-    }
+  .btn-outline-secondary:hover {
+    background-color: #202f95 !important;
+    color: #fff;
+  }
+
+  .bold {
+    font-weight: bold;
+  }
+
+  .fa-times-circle {
+    position: absolute;
+    right: 6.5rem;
+    top: 50%;
+    transform: translateY(-50%);
+  }
 }
 </style>
