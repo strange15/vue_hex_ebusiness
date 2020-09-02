755fbe00da9d1c33f38ac4b9b0f1658e5cd585d2
strange15 <strange66666666@gmail.com>
2020-09-02 18:38:09 +0800

    update build assetsPublicPath

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue_hex_ebusiness/dist/',
 
     /**
      * Source Maps
